1cf9ed5d1b200844f9f6f622372cd527eb16afef
natizui <izui.nm@gmail.com>
2018-09-25 11:56:58 -0300

    Added routes and declarations for list and form components

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { ProductFormComponent } from './products/product-form/product-form.component';
+
+const routes: Routes = [
+  { path: '', component: ProductListComponent },
+  { path: 'cadastro', component: ProductFormComponent}
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
